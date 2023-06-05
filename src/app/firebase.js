0e0d4c8b440908c3167e8240a32a627b2924a05d
macaarco <cami.arroyave89@gmail.com>
2023-06-04 19:33:10 -0500

    Actualización de home y listado de productos

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,7 +1,12 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+import {
+  getDatabase,
+  ref as refDatabase,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,10 +19,20 @@ const firebaseConfig = {
   projectId: "upb-open",
   storageBucket: "upb-open.appspot.com",
   messagingSenderId: "528037670196",
-  appId: "1:528037670196:web:24a04b01ad2468dc00c961"
+  appId: "1:528037670196:web:24a04b01ad2468dc00c961",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const rtdb = getDatabase();
+
+export const productosRef = refDatabase(rtdb, "productos");
+export const listarProductos = async () => {
+  onValue(productosRef, (snapshot) => {
+    snapshot.forEach((snap) => console.log("holaaaa"));
+  });
+};
+
+export { onValue };
--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,49 @@
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { getDocs, collection  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
-import { auth, db } from "../app/firebase.js";
+import { auth, onValue, productosRef } from "../app/firebase.js";
+import {
+  getDocs,
+  collection,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 import { loginCheck } from "../app/loginCheck.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { setupPosts } from "../app/postList.js";
 
-
-import '../app/registroForm.js'
-import '../app/signinForm.js'
-import '../app/logout.js'
+import "../app/logout.js";
+import "../app/registroForm.js";
+import "../app/signinForm.js";
 
 onAuthStateChanged(auth, async (user) => {
-    if (user){
-        const querySnapshot = await getDocs(collection(db, "post"))
-        setupPosts(querySnapshot.docs)
-    } else {
-        setupPosts([]) 
-    }
-    loginCheck(user)
-    
-})
+  if (user) {
+    // const querySnapshot = await getDocs(collection(db, "post"));
+    // setupPosts(querySnapshot.docs);
 
+    onValue(productosRef, (snapshot) => {
+      const postList = document.querySelector(".container-cursos");
+      let html = "";
+
+      snapshot.forEach((snap) => {
+        const { nombre, precio, urlImagen } = snap.val();
+
+        html += `
+        <div class="card" id="curso-card">
+            <div class="card-image" style="background-image: url(${urlImagen})"></div>
+            <h2 class="produc-title">${nombre}</h2>
+
+            <p class="produc-price">${parseInt(precio)
+              .toLocaleString("es-CO", {
+                style: "currency",
+                currency: "COP",
+              })
+              .slice(0, -3)}</p>
+
+            <a class="see-more">Ver curso</a>
+        </div>
+        `;
+      });
+
+      postList.innerHTML = html;
+    });
+  } else {
+    setupPosts([]);
+  }
+  loginCheck(user);
+});
