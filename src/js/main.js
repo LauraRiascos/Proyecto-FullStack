545d10033727853c619ecb5077df713797748baf
macaarco <cami.arroyave89@gmail.com>
2023-06-04 22:44:49 -0500

    Ajuste al listado de productos

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,20 @@
-import { auth, onValue, productosRef } from "../app/firebase.js";
-import {
-  getDocs,
-  collection,
-} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
-import { loginCheck } from "../app/loginCheck.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { setupPosts } from "../app/postList.js";
+import { auth, onValue, productosRef } from "../app/firebase.js";
+import { loginCheck } from "../app/loginCheck.js";
 
 import "../app/logout.js";
 import "../app/registroForm.js";
 import "../app/signinForm.js";
 
-onAuthStateChanged(auth, async (user) => {
-  if (user) {
-    // const querySnapshot = await getDocs(collection(db, "post"));
-    // setupPosts(querySnapshot.docs);
+window.addEventListener("DOMContentLoaded", () => {
+  onValue(productosRef, (snapshot) => {
+    const postList = document.querySelector(".container-cursos");
+    let html = "";
 
-    onValue(productosRef, (snapshot) => {
-      const postList = document.querySelector(".container-cursos");
-      let html = "";
+    snapshot.forEach((snap) => {
+      const { nombre, precio, urlImagen } = snap.val();
 
-      snapshot.forEach((snap) => {
-        const { nombre, precio, urlImagen } = snap.val();
-
-        html += `
+      html += `
         <div class="card" id="curso-card">
             <div class="card-image" style="background-image: url(${urlImagen})"></div>
             <h2 class="produc-title">${nombre}</h2>
@@ -38,12 +29,12 @@ onAuthStateChanged(auth, async (user) => {
             <a class="see-more">Ver curso</a>
         </div>
         `;
-      });
-
-      postList.innerHTML = html;
     });
-  } else {
-    setupPosts([]);
-  }
+
+    postList.innerHTML = html;
+  });
+});
+
+onAuthStateChanged(auth, (user) => {
   loginCheck(user);
 });
